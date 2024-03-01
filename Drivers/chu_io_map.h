718e464350595ca671aa8baff7841e561a58f08b
24x7fpga <kiran.24x7fpga@gmail.com>
2024-03-01 01:04:08 -0500

    Drivers: changed clk to 125MHz to fucntion with ZyboZ7-20

--- Drivers/chu_io_map.h
+++ Drivers/chu_io_map.h
@@ -1,11 +1,13 @@
 #ifndef _CHU_IO_MAP_INCLUDED
 #define _CHU_IO_MAP_INCLUDED
 
+#include <inttypes.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define SYS_CLK_FREQ 100
+#define SYS_CLK_FREQ 125
 
 // io base address for microblaze MCS (FRPO bridge address)
 #define BRIDGE_BASE 0xc0000000
@@ -13,9 +15,9 @@ extern "C" {
 // slot machine definition
 // format: SLOT#_ModuleType_Name
 #define SO_SYS_TIME 0
-#define SO_UART1    1
-#define SO_LED      2
-#define SO_SW       3
+#define S1_UART1    1
+#define S2_LED      2
+#define S3_SW       3
 
 #ifdef __cplusplus
 }
